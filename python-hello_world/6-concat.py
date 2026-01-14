45a0c544f5c6ba6ad4364ca32fb93794896537d5
anil-akr <4nil000@protonmail.com>
2026-01-14 09:29:06 +0000

    edit 1000

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-# Concatenate str1 and str2 in the print statement
-print("Welcome to {} {}!".format(str1, str2))
+# print the concatenated string
+print("Welcome to " + str1 + " " + str2 + "!")
