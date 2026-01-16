857d4b20f99d9197d4e9cbc765cfd53064fccbca
anil-akr <4nil000@protonmail.com>
2026-01-16 13:55:38 +0000

    Fix

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[39:])  # slice from the start of "object-oriented programming with Python" to the end
+# Take only "object-oriented programming with python"
+print(str[48:72] + str[0:6])
