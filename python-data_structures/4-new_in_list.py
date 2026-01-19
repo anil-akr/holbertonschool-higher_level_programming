8842e5434359fa7e552248f552656dc6f97c3919
anil-akr <4nil000@protonmail.com>
2026-01-19 11:01:43 +0000

    T4 chmoed

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    new_list = my_first.copy()
+    new_list = my_list.copy()
 
     if idx < 0 or idx >= len(my_list):
         return new_list
