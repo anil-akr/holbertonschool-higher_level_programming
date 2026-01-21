ebf644ec3294fbe6b7c05ee1a5fa78e7e26e57cd
anil-akr <4nil000@protonmail.com>
2026-01-21 09:32:16 +0000

    Fix pycodestyle for T8

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -3,10 +3,12 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) > 0:
         a0 = tuple_a[0]
-    else: a0 = 0
+    else:
+        a0 = 0
     if len(tuple_a) > 1:
         a1 = tuple_a[1]
-    else: a1 = 0
+    else:
+        a1 = 0
 
     if len(tuple_b) > 0:
         b0 = tuple_b[0]
