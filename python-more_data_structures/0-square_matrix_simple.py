7dfd9ae52e5a4b924b502ce1e1da0af8fbc065d0
anil-akr <4nil000@protonmail.com>
2026-01-21 14:01:10 +0000

    m

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -5,8 +5,8 @@ def square_matrix_simple(matrix=[]):
 
     for row in matrix:
         new_row = []
-    for value in row:
-        new_row.append(value * value)
-    new_matrix.append(new_row)
+        for value in row:
+            new_row.append(value * value)
+        new_matrix.append(new_row)
 
     return new_matrix
