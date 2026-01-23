b8a8b567306fbe979ae6d7c0ce176664849d825d
anil-akr <4nil000@protonmail.com>
2026-01-23 22:57:47 +0000

    ?

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """Test cases for max_integer function"""
+
+    def test_ordered_list(self):
+        """Test with an ordered list"""
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_unordered_list(self):
+        """Test with an unordered list"""
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+
+    def test_max_at_beginning(self):
+        """Test when the max is at the beginning"""
+        self.assertEqual(max_integer([4, 1, 2, 3]), 4)
+
+    def test_empty_list(self):
+        """Test an empty list"""
+        self.assertIsNone(max_integer([]))
+
+    def test_one_element_list(self):
+        """Test list with one element"""
+        self.assertEqual(max_integer([7]), 7)
+
+    def test_negative_numbers(self):
+        """Test list with negative numbers"""
+        self.assertEqual(max_integer([-1, -3, -2, -4]), -1)
+
+    def test_floats(self):
+        """Test list with floats"""
+        self.assertEqual(max_integer([1.5, 2.5, 0.5]), 2.5)
+
+    def test_mixed_int_float(self):
+        """Test list with ints and floats"""
+        self.assertEqual(max_integer([1, 2.5, 3, 0.5]), 3)
+
+    def test_repeated_numbers(self):
+        """Test list with repeated max"""
+        self.assertEqual(max_integer([3, 3, 3]), 3)
+
+    def test_large_list(self):
+        """Test with a large list"""
+        self.assertEqual(max_integer(list(range(1000))), 999)
+
+    def test_list_with_one_negative(self):
+        """Test list with one negative number"""
+        self.assertEqual(max_integer([-10]), -10)
+
+
+if __name__ == '__main__':
+    unittest.main()
