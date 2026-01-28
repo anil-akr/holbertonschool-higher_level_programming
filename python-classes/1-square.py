bec92b79094b3b3f82ef3660b408d1798326fd39
anil-akr <4nil000@protonmail.com>
2026-01-28 08:59:29 +0000

    ?

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Module that defines a Square class"""
+
 
 class Square:
+    """Class that defines a square"""
+
     def __init__(self, size):
+        """Initialize a square with a given size"""
         self.__size = size
