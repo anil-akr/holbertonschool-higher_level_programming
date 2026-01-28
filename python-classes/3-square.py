1a7192b4918093cf1f93dbf985f9706d945e709b
anil-akr <4nil000@protonmail.com>
2026-01-28 09:38:50 +0000

    T3 done

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -4,11 +4,14 @@
 
 class Square:
     """Class that defines a square"""
-
     def __init__(self, size=0):
-        """Initialize a square with size validation"""
+        """Initialize a square with validated size"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError("size must be >= 0")
+            raise TypeError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        """Return the current square area"""
+        return self.__size ** 2
