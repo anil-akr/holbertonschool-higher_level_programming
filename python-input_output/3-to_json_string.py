7e06af0e1fd6a280b9549c2c6f5780db2ee3a2cd
anil-akr <4nil000@protonmail.com>
2026-02-12 12:50:10 +0000

    T3 fix

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Module that defines a function to convert a Python object to a JSON string."""
+"""Module that defines a function to convert a Python object
+to a JSON string."""
 
 import json
 
