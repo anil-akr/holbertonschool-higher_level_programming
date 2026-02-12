7da0c80a97b22c62a2c907d3a30ca8bb40f4d92a
anil-akr <4nil000@protonmail.com>
2026-02-12 12:48:20 +0000

    T2 mh

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,6 +3,7 @@
 
 
 def append_write(filename="", text=""):
-    """Appends a string to a UTF-8 text file and returns the number of characters written."""
+    """Appends a string to a UTF-8 text file and returns
+    the number of characters written."""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
