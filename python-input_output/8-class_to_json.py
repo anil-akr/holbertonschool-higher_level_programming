cdadeea253c261776a05d9bc23be7457336b3f06
anil-akr <4nil000@protonmail.com>
2026-02-12 09:22:48 +0000

    T8 pycodestyle

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
-"""Module that defines a function to convert a class instance to a dictionary for JSON serialization."""
+"""Module that defines a function to convert a class instance
+to a dictionary for JSON serialization.
+"""
 
 
 def class_to_json(obj):
-    """Returns the dictionary representation of a class instance attributes."""
+    """Return the dictionary representation of a class instance attributes.
+
+    Args:
+        obj (object): The class instance.
+
+    Returns:
+        dict: Dictionary of the instance's attributes.
+    """
     return obj.__dict__
