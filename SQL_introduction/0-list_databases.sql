f832c8633bce16fd4b3143a31d547c6614ea62c2
anil-akr <4nil000@protonmail.com>
2026-03-05 01:41:25 +0000

    Fixed for style

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,1 +1,2 @@
+-- Lists all databases of mysql server
 SHOW DATABASES;
--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,1 +1,3 @@
-DROP DATABASE hbtn_0c_0;
+-- Deletes the database hbtn_0c_0 in your MySQL server
+-- If the database hbtn_0c_0 doesn't exist, the script should not fail
+DROP DATABASE IF EXISTS hbtn_0c_0;
