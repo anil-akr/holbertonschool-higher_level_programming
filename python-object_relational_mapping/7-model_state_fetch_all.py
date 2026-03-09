6b65e7436724a8f9b485bba8af55f692c6aaffb3
anil-akr <4nil000@protonmail.com>
2026-03-09 17:30:33 +0000

    T7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,18 +9,19 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        msg = ("Usage: ./7-model_state_fetch_all.py "
-       "<username> <password> <database>")
-        print(msg)
+        print(
+            "Usage: ./7-model_state_fetch_all.py "
+            "<username> <password> <database>"
+        )
         sys.exit(1)
 
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    # Création de l’engine avec URL multi-lignes pour respecter PEP8
+    # Création de l’engine
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
             username, password, database
         ),
         pool_pre_ping=True
