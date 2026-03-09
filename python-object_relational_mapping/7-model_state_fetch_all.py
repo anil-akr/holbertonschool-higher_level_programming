fcc4cccfbfe8bfaf699da385c16a5f0eee9d9955
anil-akr <4nil000@protonmail.com>
2026-03-09 17:25:08 +0000

    m

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Lists all State objects from the database hbtn_0e_6_usa
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    # Vérification des arguments (optionnelle, mais utile pour éviter IndexError)
+    if len(sys.argv) != 4:
+        print("Usage: ./7-model_state_fetch_all.py <username> <password> <database>")
+        sys.exit(1)
+
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    # Création de l’engine
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(username, password, database),
+        pool_pre_ping=True
+    )
+
+    # Création d’une session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Récupération de tous les états triés par id
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    session.close()
