041a3dab9ce3e5432ca95fc5e22304d674caa157
anil-akr <4nil000@protonmail.com>
2026-06-08 12:55:15 +0000

    T4

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Script that lists all cities from the database hbtn_0e_4_usa
+with their corresponding state name, using a JOIN query."""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
+
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.id, cities.name, states.name "
+    "FROM cities JOIN states ON cities.state_id = states.id "
+    "ORDER BY cities.id ASC;")
+    rows = cursor.fetchall()
+
+    for x in rows:
+        print(x)
+
+    cursor.close()
+    db.close()
