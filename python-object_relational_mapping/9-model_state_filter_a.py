8f72413cfa6cdc4a5236000b2d010eb1dc6fd58c
anil-akr <4nil000@protonmail.com>
2026-06-10 07:47:41 +0000

    Fix ppycodestyle T9

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
     db_name = sys.argv[3]
 
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'
-            .format(username, password, db_name),
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            username, password, db_name),
         pool_pre_ping=True
     )
 
@@ -21,9 +21,9 @@ if __name__ == "__main__":
     session = Session()
 
     states = (session.query(State)
-                    .filter(State.name.like("%a%"))
-                    .order_by(State.id)
-                    .all())
+              .filter(State.name.like("%a%"))
+              .order_by(State.id)
+              .all())
 
     for state in states:
         print("{}: {}".format(state.id, state.name))
